d0bb1f9dd0d956d1ddd45acda0d147b70433243c
Jarquise Turner <j.turner348@gmail.com>
2021-10-27 01:30:40 -0400

    Create SoldierService.js

---
+++ capstone-frontend/src/service/SoldierService.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+// I need to finish back end
+// const Mangement_system
+//This is the component with the errors before the code broke after I create the listSoldierComponent
+
+const
+
+export default class SoldierService {
+
+    getSoldier(){
+        return axios.get(soldiers_mangement_system)
+    }
+}
+
+export default new SoldierService
