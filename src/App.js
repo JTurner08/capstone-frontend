2829c5062d1a1dfde6e37367f69654d8de0003a3
Jarquise Turner <j.turner348@gmail.com>
2021-10-27 17:26:24 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import {BrowserRouter as Router,Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 
 import Header from './components/Header';
 import Footer from './components/Footer';
