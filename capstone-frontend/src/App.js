66ddd0cd08212d7d108080dce512a5c49d20ef4c
Jarquise Turner <j.turner348@gmail.com>
2021-10-27 01:29:32 -0400

    Create App.js

---
+++ capstone-frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ListSoldierComponent from './components/ListSoldierComponent'
+
+function App() {
+  return (
+    <div className="container">
+       <ListSoldierComponent />
+    </div>
+  );
+}
+
+export default App;
