a78fa19ea706254b2212d04911343379f9347392
Jarquise Turner <j.turner348@gmail.com>
2021-10-27 13:10:56 -0400

    Create SoldierService.js

---
+++ src/service/SoldierService.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+// backend won't connect?
+// const soldier_mangement_system
+//This is the component with the errors before the code broke after I create the listSoldier
+
+const
+
+export default class SoldierService {
+
+    getSoldier(){
+        return axios.get(soldiers_mangement_system)
+    }
+}
+
+export default new SoldierService
