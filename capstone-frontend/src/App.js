efbc0dd6c93901dd884d4ff12e04fd5e3987f75d
Jarquise Turner <j.turner348@gmail.com>
2021-10-27 11:40:35 -0400

    Update App.js

--- capstone-frontend/src/App.js
+++ capstone-frontend/src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import ListSoldierComponent from './components/ListSoldierComponent'
+import {BrowserRouter as Router,Route, Switch} from 'react-router-dom';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import ListSoldier from './components/ListSoldier';
+import AddSoldier from './components/AddSoldier';
+import UpdateSoldier from './components/UpdateSoldier';
+import DeleteSoldier from './components/DeleteSoldier';
+import ViewSoldier from './components/ViewSoldier';
 
 function App() {
-  return (
-    <div className="container">
-       <ListSoldierComponent />
-    </div>
+    return (
+        <div>
+          <Router>
+          <Header />
+            <div className="container">
+              <Switch>
+                  <Route path="/" exact component={ListSoldier}></Route>
+                  <Route path="/soldiers" component={ListSoldier}></Route>
+                  <Route path="/add-soldier" component={AddSoldier}></Route>
+                  <Route path="/update-soldier/:id" component={UpdateSoldier}></Route> 
+                  <Route path="/delete-soldier/:id" component={DeleteSoldier}></Route> 
+                  <Route path="/view-soldier/:id" component={ViewSoldier}></Route> 
+                  
+              </Switch>
+            </div>
+            <Footer />
+            
+          </Router>
+        </div>
   );
 }
 
