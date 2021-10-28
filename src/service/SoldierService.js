352c85a13b13f7c831e06b2a27ef53365f3da639
Jarquise Turner <j.turner348@gmail.com>
2021-10-28 03:14:49 -0400

    Update SoldierService.js

--- src/service/SoldierService.js
+++ src/service/SoldierService.js
@@ -5,26 +5,28 @@ const SOLDIER_API_BASE_URL = "http://localhost:8080/api"
 class SoldierService {
 
     getSoldiers(){
-        return axios.get(SOLDIER_API_BASE_URL + '/soldier')
+        return axios.get(SOLDIER_API_BASE_URL + '/soldiers')
     }
 
 createSoldier(soldier){
-    return axios.post(SOLDIER_API_BASE_URL + '/add-soldier' +soldier)
+    return axios.post(SOLDIER_API_BASE_URL + '/add-soldier', soldier)
 }
 
 getSoldierByID(soldierId){
-    return axios.get(SOLDIER_API_BASE_URL + '/soldier' + soldierId)
+    return axios.get(SOLDIER_API_BASE_URL + '/soldier/' + soldierId)
 }
 
-updateSoldier(soldierId){
-    return axios.put(SOLDIER_API_BASE_URL + '/soldiers' + soldierId)
-
-}
-deleteSoldier(soldierId){
-    return axios.delete(SOLDIER_API_BASE_URL + '/soldiers' + soldierId)
-
+updateSoldier(soldier,id){
+    return axios.put(SOLDIER_API_BASE_URL+"/soldier/"+id,soldier);
 }
 
+
+
+deleteSoldier(soldierId) {
+    return axios.delete(SOLDIER_API_BASE_URL + '/soldiers/' + soldierId)
+
 }
 
-export default new SoldierService
+}
+
+export default new SoldierService();
