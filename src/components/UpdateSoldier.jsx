4b57160a4ff66e39828d2c73b35b94f723b06a81
Jarquise Turner <j.turner348@gmail.com>
2021-10-28 09:47:56 -0400

    Update UpdateSoldier.jsx

--- src/components/UpdateSoldier.jsx
+++ src/components/UpdateSoldier.jsx
@@ -1,26 +1,32 @@
 import React, { Component } from 'react'
 import SoldierService from '../service/SoldierService';
 
+
+
 class UpdateSoldier extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
             id: this.props.match.params.id,
-            name: '',
+            firstName: '',
+            lastName: '',
             rank: '',
             skill: ''
         }
-        this.nameHandler = this.nameHandler.bind(this);
+        this.idHandler=this.idHandler.bind(this);
+        this.firstnameHandler = this.firstnameHandler.bind(this);
+        this.lastnameHandler = this.lastnameHandler.bind(this);
         this.rankHandler = this.rankHandler.bind(this);
         this.skillHandler = this.skillHandler.bind(this);
     }
 
-    componentDidMount(){
-        SoldierService.getSoldierById(this.state.id).then( (res) =>{
+     componentDidMount(){
+        SoldierService.getSoldierByID(this.state.id).then( (res) =>{
             let soldier = res.data;
             this.setState({
-                name: soldier.name,
+                firstName: soldier.firstname,
+                lastName: soldier.lastname,
                 rank : soldier.rank,
                 skill: soldier.skill
 
@@ -30,16 +36,22 @@ class UpdateSoldier extends Component {
 
     updateSoldier = (e) => {
         e.preventDefault();
-        let soldier = {name: this.state.name, rank: this.state.rank, skill: this.state.skill};
+        let soldier = {firstname: this.state.firstname, lastname:this.state.lastname, rank: this.state.rank, skill: this.state.skill};
         console.log('soldier => ' + JSON.stringify(soldier));
         console.log('id => ' + JSON.stringify(this.state.id));
         SoldierService.updateSoldier(soldier, this.state.id).then( res => {
             this.props.history.push('/soldiers');
         });
     }
-    
-    nameHandler= (event) => {
-        this.setState({firstName: event.target.value});
+    idHandler= (event) => {
+        this.setState({id: event.target.value});
+    } 
+    firstnameHandler= (event) => {
+        this.setState({firstname: event.target.value});
+    }
+
+    lastnameHandler= (event) => {
+        this.setState({lastname: event.target.value});
     }
 
     rankHandler= (event) => {
@@ -64,15 +76,25 @@ class UpdateSoldier extends Component {
                                 <div className = "card-body">
                                     <form>
                                         <div className = "form-group">
-                                            <label> Name: </label>
+                                            <label> firstname: </label>
                                             <input placeholder="name" name="name" className="form-control" 
-                                                value={this.state.name} onChange={this.nameHandler}/>
+                                                value={this.state.firstName} onChange={this.firstNameHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label> lastname: </label>
+                                            <input placeholder="name" name="name" className="form-control" 
+                                                value={this.state.lastName} onChange={this.lastNamehandler}/>
                                         </div>
                                         
                                         <div className = "form-group">
                                             <label> Rank: </label>
                                             <input placeholder="Rank" name="rank" className="form-control" 
                                                 value={this.state.rank} onChange={this.rankHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label> Skill: </label>
+                                            <input placeholder="Rank" name="rank" className="form-control" 
+                                                value={this.state.skill} onChange={this.skillHandler}/>
                                         </div>
                                         
 
@@ -89,4 +111,4 @@ class UpdateSoldier extends Component {
     }
 }
 
-export default UpdateSoldier;
+export default UpdateSoldier
